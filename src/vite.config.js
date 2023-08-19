5b6deecb1be003b1a9c3b06958581a398edd0f9e
Igor Karplyuk <karprog01@gmail.com>
2023-08-19 16:42:50 +0300

    laravel frontend serve url

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+        hmr: {host: 'localhost'},
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
